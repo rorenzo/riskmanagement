0955b487be961d47f9560a4448817ac6c3648840
rorenzo <roreapak@gmail.com>
2025-05-29 07:36:27 +0000

    route fix

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
 })->middleware('auth');
 
 require __DIR__.'/portal.php';
